33790eeb5a49b8f89564930ae445c80c5080ca7b
Arun <arunmundakkal003@gmail.com>
2026-02-21 21:20:12 +0530

    st

---
+++ src/main/java/com/docker/strudent/StudentController.java
@@ -0,0 +1,17 @@
+package com.docker.strudent;
+
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    public List<Students> getStudents() {
+        return List.of(
+            new Student(1, "ram", 22),
+            new Student(2, "jhon", 26),
+            new Student(3, "Alice", 18)
+        );
+    }
+
+}
