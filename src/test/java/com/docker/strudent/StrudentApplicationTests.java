3830aed7f4698a0cd653f24790192cf37ee20a8e
Arun <arunmundakkal003@gmail.com>
2026-02-21 21:02:37 +0530

    ini

---
+++ src/test/java/com/docker/strudent/StrudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.strudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
